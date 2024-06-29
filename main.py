6bb6031c12c4972fa80cee02b1d5e544c7ccac65
socalledtheraven <tom.dalrymple3@gmail.com>
2024-06-29 16:07:57 +0100

    theoretically complete, needs testing

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ receiver_email = "cassie.dalrymple3@gmail.com"
 
 
 def get_hot_posts_for_subreddit(subreddit_name):
+    print("Getting hot posts for " + subreddit_name)
     r = requests.get(f"https://www.reddit.com/r/{subreddit_name}/hot.json")
     data = r.json()["data"]["children"]
 
@@ -24,86 +25,99 @@ def get_hot_posts_for_subreddit(subreddit_name):
             posts_data.append(data[i]["data"])
             # separate post count and index bc filtering posts
             posts += 1
+            print("post number " + str(posts))
         i += 1
 
-    # not permanent, just for caching
-    with open(f"{subreddit_name}.json", "w+") as f:
-        json.dump(posts_data, f, indent=4)
+    # # not permanent, just for caching
+    # with open(f"{subreddit_name}.json", "w+") as f:
+    #     json.dump(posts_data, f, indent=4)
 
-    return posts_data
+    return {"data": posts_data, "subreddit_url": f"https://www.reddit.com/r/{subreddit_name}"}
 
 
-def create_email(posts_data):
+def create_email(full_data):
     # grabs basic structure
     with open("email.html", "r") as f:
         soup = BeautifulSoup(f.read(), features="html.parser")
 
     body = soup.find("body")
 
-    # adds each post at the time
-    for post in posts_data:
-        # creates link
-        a = soup.new_tag("a", href="https://www.reddit.com" + post["permalink"])
-        a.string = post["title"]
+    for i in range(full_data):
+        subreddit_data = full_data[i]["data"]
 
-        # creates title
-        h2 = soup.new_tag("h2")
-        h2.append(a)
+        # creates the subreddit title
+        a = soup.new_tag("a", href=full_data[i]["subreddit_url"])
+        a.string = "".join(full_data[i]["subreddit_url"].split("/")[-2:])
 
-        body.append(h2)
+        h1 = soup.new_tag("h1")
+        h1.append(a)
 
-        # adds text
-        if post["selftext_html"] is not None:
-            contents = BeautifulSoup(html.unescape(post["selftext_html"]), features="html.parser")
-            div = contents.find("div")
-            div["style"] = "padding-left: 4em"
-            body.append(contents)
+        body.append(h1)
 
-        # adds image
-        if "preview" in post:
-            # for some reason, reddit will sometimes give a direct link and sometimes not. this is a redirect that still seems to work
-            if "https://i.redd.it" in post["url_overridden_by_dest"]:
-                img = soup.new_tag("img", src=post["url_overridden_by_dest"])
+        # goes through each post
+        for post in subreddit_data:
+            # creates link
+            a = soup.new_tag("a", href="https://www.reddit.com" + post["permalink"])
+            a.string = post["title"]
 
-                # adds padding to fit with the text
-                if post["selftext_html"] is not None:
-                    img["style"] = "padding-left: 4em"
-                body.append(img)
+            # creates title
+            h2 = soup.new_tag("h2")
+            h2.append(a)
 
-            elif "https://v.redd.it" in post["url_overridden_by_dest"]:
-                p = soup.new_tag("p", "")
-                p.string = "video"
+            body.append(h2)
 
-                # adds padding to fit with the text
-                if post["selftext_html"] is not None:
-                    p["style"] = "padding-left: 4em"
+            # adds text
+            if post["selftext_html"] is not None:
+                contents = BeautifulSoup(html.unescape(post["selftext_html"]), features="html.parser")
+                div = contents.find("div")
+                div["style"] = "padding-left: 8em"
+                body.append(contents)
 
-                body.append(p)
+            # adds image
+            if "preview" in post:
+                # for some reason, reddit will sometimes give a direct link and sometimes not. this is a redirect that still seems to work
+                if "https://i.redd.it" in post["url_overridden_by_dest"]:
+                    img = soup.new_tag("img", src=post["url_overridden_by_dest"])
 
-        # handles gallery posts
-        if "gallery_data" in post:
-            urls = get_gallery_image_urls("https://www.reddit.com" + post["permalink"])
+                    # adds padding to fit with the text
+                    if post["selftext_html"] is not None:
+                        img["style"] = "padding-left: 8em"
+                    body.append(img)
 
-            for url in urls:
-                img = soup.new_tag("img", src=url)
+                elif "https://v.redd.it" in post["url_overridden_by_dest"]:
+                    p = soup.new_tag("p", "")
+                    p.string = "video"
 
-                # adds padding to fit with the text
-                if post["selftext_html"] is not None:
-                    img["style"] = "padding-left: 4em"
-                body.append(img)
+                    # adds padding to fit with the text
+                    if post["selftext_html"] is not None:
+                        p["style"] = "padding-left: 8em"
 
-        # gets the icon, colour of text and resizes it all to fit
-        comments_icon = soup.new_tag("img", src="reddit_comment.png", width="13px")
-        comments_text = soup.new_tag("p", style="color: #999999 !important;")
-        num_comments = post["num_comments"]
+                    body.append(p)
 
-        if num_comments != 1:
-            comments_text.string = f"{num_comments} Comments"
-        else:
-            comments_text.string = f"{num_comments} Comment"
+            # handles gallery posts
+            if "gallery_data" in post:
+                urls = get_gallery_image_urls("https://www.reddit.com" + post["permalink"])
 
-        comments_text.insert(0, comments_icon)
-        body.append(comments_text)
+                for url in urls:
+                    img = soup.new_tag("img", src=url)
+
+                    # adds padding to fit with the text
+                    if post["selftext_html"] is not None:
+                        img["style"] = "padding-left: 8em"
+                    body.append(img)
+
+            # gets the icon, colour of text and resizes it all to fit
+            comments_icon = soup.new_tag("img", src="https://www.redditstatic.com/emaildigest/reddit_comment.png", width="13px")
+            comments_text = soup.new_tag("p", style="color: #999999 !important;")
+            num_comments = post["num_comments"]
+
+            if num_comments != 1:
+                comments_text.string = f"{num_comments} Comments"
+            else:
+                comments_text.string = f"{num_comments} Comment"
+
+            comments_text.insert(0, comments_icon)
+            body.append(comments_text)
 
     return soup.prettify()
 
@@ -146,11 +160,13 @@ def send_email(html):
 
 
 def main():
-    # get_hot_posts_for_subreddit("feedthebeast")
-    with open("feedthebeast.json", "r") as f:
-        data = json.load(f)
-    # create_email(data)
-    create_email(data)
+    subreddits = ["feedthebeast", "singularity", "obsidianmd"]
+    full_data = []
+    for subreddit in subreddits:
+        full_data.append(get_hot_posts_for_subreddit(subreddit))
+
+    email = create_email(full_data)
+    send_email(email)
 
 
 if __name__ == "__main__":
